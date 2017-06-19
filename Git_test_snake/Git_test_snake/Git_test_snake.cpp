a0fc23f501ec4ecb0f37723e0d52834e3f6ddde9
geyuan001 <2277150852@qq.com>
2017-06-19 09:43:48 +0800

    snake
    snake

---
+++ Git_test_snake/Git_test_snake/Git_test_snake.cpp
@@ -0,0 +1,15 @@
+// Git_test_snake.cpp : 定义控制台应用程序的入口点。
+//
+#include <iostream>
+#include"stdafx.h"
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main()
+{
+	printf("Hello world");
+	puts("Hello world");
+	cout << "Hello world" << endl;
+	return 0;
+}
